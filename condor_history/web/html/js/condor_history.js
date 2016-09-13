be9a4f7299f087befa629efad85cf37dd0474160
Yutaro Iiyama <yiiyama@mit.edu>
2016-09-13 16:26:20 -0400

    can show a job table now

--- condor_history/web/html/js/condor_history.js
+++ condor_history/web/html/js/condor_history.js
@@ -1,72 +1,196 @@
-var findClusterBlocked = false;
-
-function findClustersWith(key)
-{
-    if (findClusterBlocked)
-        return;
-
-    findClusterBlocked = true;
-    setTimeout(function () { findClusterBlocked = false; }, 500);
-
-    var key = $('#' + key);
-    var str = key.val();
-    var search = str.split(' ');
-    search.sort();
-    
-    var current = key.data('keys');
-    if (current === undefined)
-        current = [];
-
-    if (current.length == search.length) {
-        var x = 0;
-        for (x = 0; x != current.length; ++x) {
-            if (search[x] != current[x])
-                break;
-        }
-        if (x == current.length)
-            return;
-    }
-
-    key.data('keys', search);
-
-    loadClusters();
-}
-
-function loadClusters()
+function initPage()
 {
     var inputData = {
-        'search': 'clusters',
-        'users': $('#users').data('keys'),
-        'cmds': $('#cmds').data('keys')
+        'search': 'users'
     };
 
     $.get('search.php', inputData, function (data, textStatus, jqXHR) {
+            displayUsers(data);
+        }, 'json');
+
+    var table = d3.select('#clusterList')
+        .style('height', '42px');
+
+    var tableNode = table.node();
+
+    var headRow = table.select('thead').append('tr');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.05 - 1) + 'px')
+        .append('input')
+        .attr({'type': 'checkbox', 'id': 'selectAllClusters'})
+        .on('change', function () {
+                var check = d3.selectAll('#clusterList tbody tr td.select input');
+                check.property('checked', d3.select(this).property('checked'));
+                loadJobs();
+            });
+
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.15 - 1) + 'px')
+        .text('ClusterId');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.15 - 1) + 'px')
+        .text('User');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.3 - 1) + 'px')
+        .text('Executable');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.2 - 1) + 'px')
+        .text('Submit date');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.15 - 1) + 'px')
+        .text('Jobs');
+
+    table = d3.select('#jobList')
+        .style('height', '42px');
+
+    tableNode = table.node();
+
+    headRow = table.select('thead').append('tr');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.08 - 1) + 'px')
+        .text('ClusterId');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.08 - 1) + 'px')
+        .text('ProcId');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.08 - 1) + 'px')
+        .text('User');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.08 - 1) + 'px')
+        .text('Executable');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.13 - 1) + 'px')
+        .text('Start time');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.05 - 1) + 'px')
+        .text('Succeeded');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.2 - 1) + 'px')
+        .text('CPU time');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.2 - 1) + 'px')
+        .text('Wallclock time');
+    headRow.append('th')
+        .style('width', (tableNode.clientWidth * 0.1 - 1) + 'px')
+        .text('Exit code');
+}
+
+function displayUsers(users)
+{
+    var box = d3.select('#users');
+    var textHeight = parseInt(window.getComputedStyle(box.node()).fontSize, 10);
+    if (users.length > 5) {
+        box.style('height', (textHeight * 6) + 'px');
+    }
+    else
+        box.style('height', (textHeight * (users.length + 1)) + 'px');
+
+    var lines = box.selectAll('div.user')
+        .data(users)
+        .enter().append('div').classed('user', true);
+
+    lines.append('input').attr('type', 'checkbox')
+        .property('value', function (d) { return d.name; })
+        .on('change', function () { findClusters(); });
+
+    lines.append('span')
+        .text(function (d) { return d.name; });
+}
+
+var findClusterBlocked = false;
+var clusterSearchKeys = {
+    'search': 'clusters',
+    'users': [],
+    'cmds': []
+};
+
+function findClusters()
+{
+    if (findClusterBlocked) {
+        setTimeout(function () { findClusters(); }, 1000);
+        return;
+    }
+
+    findClusterBlocked = true;
+    setTimeout(function () { findClusterBlocked = false; }, 1000);
+
+    var search = {'users': [], 'cmds': []};
+
+    d3.select('#users').selectAll('input')
+        .each(function () {
+                if (this.checked)
+                    search.users.push(this.value);
+            });
+
+    var cmdstr = $.trim(d3.select('#cmds').property('value'));
+    if (cmdstr != '') {
+        search.cmds = cmdstr.split(' ');
+        search.cmds.sort();
+    }
+
+    var changed = false;
+    for (var key in search) {
+        current = clusterSearchKeys[key];
+        if (current.length == search[key].length) {
+            var x = 0;
+            for (x = 0; x != current.length; ++x) {
+                if (search[key][x] != current[x])
+                    break;
+            }
+            if (x == current.length)
+                continue;
+        }
+
+        changed = true;
+    }
+
+    if (!changed)
+        return;
+
+    clusterSearchKeys.users = search.users;
+    clusterSearchKeys.cmds = search.cmds;
+
+    var spinner = new Spinner({'scale': 5, 'corners': 0, 'width': 2, 'position': 'relative', 'top': '-50%', 'left': '50%'});
+    spinner.spin();
+    $('#clusterSelect').append($(spinner.el));
+
+    $.get('search.php', clusterSearchKeys, function (data, textStatus, jqXHR) {
             showClusters(data);
+            spinner.stop();
         }, 'json');
 }
 
 function showClusters(data)
 {
-    var rows = d3.select('#clusterList tbody').selectAll('tr')
+    var table = d3.select('#clusterList');
+
+    var rows = table.select('tbody').selectAll('tr')
         .remove()
         .data(data)
         .enter()
         .append('tr')
         .attr('id', function (d) { return 'cluster' + d.cid; });
-        
+
+    var tableNode = table.node();
+
     rows.append('td')
         .classed('select', true)
+        .style({'width': (tableNode.clientWidth * 0.05 - 1) + 'px', 'padding': 0})
         .append('input')
         .attr('type', 'checkbox')
         .on('change', function () { loadJobs(); });
     rows.append('td')
         .classed('clusterId', true)
+        .style('width', (tableNode.clientWidth * 0.15 - 11) + 'px')
         .text(function (d) { return d.cid; });
     rows.append('td')
+        .style('width', (tableNode.clientWidth * 0.15 - 11) + 'px')
         .text(function (d) { return d.user; });
     rows.append('td')
+        .style('width', (tableNode.clientWidth * 0.3 - 11) + 'px')
         .text(function (d) { return d.cmd; });
     rows.append('td')
+        .style('width', (tableNode.clientWidth * 0.2 - 11) + 'px')
         .text(function (d) { return d.timestamp; });
     rows.append('td')
         .text(function (d) { return d.njobs; });
@@ -74,13 +198,19 @@ function showClusters(data)
 
 function loadJobs()
 {
+    d3.select('#jobList tbody').selectAll('tr')
+        .remove();
+
     var clusterIds = [];
     d3.selectAll('#clusterList tbody tr')
         .each(function () {
                 var thissel = d3.select(this);
-                if (thissel.select('.select input').property('checked'))
+                if (thissel.select('td.select input').property('checked'))
                     clusterIds.push(0 + thissel.select('.clusterId').text());
             });
+
+    if (clusterIds.length == 0)
+        return;
     
     var spinner = new Spinner({'scale': 5, 'corners': 0, 'width': 2, 'position': 'absolute'});
     spinner.spin();
@@ -100,25 +230,41 @@ function loadJobs()
 function showJobs(data, area)
 {
     if (area == 'table') {
-        var rows = d3.selectAll('jobTable tbody tr')
+        var table = d3.select('#jobList');
+        var rows = table.select('tbody').selectAll('tr')
             .remove()
             .data(data)
             .enter()
             .append('tr');
 
+        var tableNode = table.node();
+
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.08 - 11) + 'px')
             .text(function (d) { return d.cid; });
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.08 - 11) + 'px')
             .text(function (d) { return d.pid; });
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.08 - 11) + 'px')
+            .text(function (d) { return d.user; });
+        rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.08 - 11) + 'px')
+            .text(function (d) { return d.cmd; });
+        rows.append('td')
+            .style({'width': (tableNode.clientWidth * 0.13 - 11) + 'px', 'text-align': 'center'})
             .text(function (d) { return d.matchTime; });
         rows.append('td')
+            .style({'width': (tableNode.clientWidth * 0.05 - 11) + 'px', 'text-align': 'center'})
             .text(function (d) { return d.success; });
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.2 - 11) + 'px')
             .text(function (d) { return d.cputime; });
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.2 - 11) + 'px')
             .text(function (d) { return d.walltime; });
         rows.append('td')
+            .style('width', (tableNode.clientWidth * 0.1 - 11) + 'px')
             .text(function (d) { return d.exitcode; });
     }
 }
--- condor_history/web/html/search.php
+++ condor_history/web/html/search.php
@@ -13,9 +13,22 @@ $stmt->close();
 
 $data = array();
 
-if ($_REQUEST['search'] == 'clusters') {
+if ($_REQUEST['search'] == 'users') {
+  $stmt = $db->prepare('SELECT `name` FROM `users` WHERE `user_id` != 0');
+  $stmt->bind_result($name);
+  $stmt->execute();
+  while ($stmt->fetch())
+    $data[] = array("name" => $name);
+  $stmt->close();
+}
+else if ($_REQUEST['search'] == 'clusters') {
   $users = $_REQUEST['users'];
   $cmds = $_REQUEST['cmds'];
+
+  if (count($users) == 0 && count($cmds) == 0) {
+    echo json_encode($data);
+    exit(0);
+  }
 
   if (is_array($users)) {
     $quoted_users = array();
@@ -27,8 +40,10 @@ if ($_REQUEST['search'] == 'clusters') {
 
   if (is_array($cmds)) {
     $quoted_cmds = array();
-    foreach ($cmds as $cmd)
-      $quoted_cmds[] = sprintf("'%s'", $db->real_escape_string($cmd));
+    foreach ($cmds as $cmd) {
+      if (strlen($cmd) != 0)
+        $quoted_cmds[] = sprintf("'%s'", $db->real_escape_string($cmd));
+    }
   }
   else if ($cmds != "")
     $quoted_cmds = array(sprintf("'%s'", $cmds));
@@ -49,30 +64,34 @@ if ($_REQUEST['search'] == 'clusters') {
   $stmt->execute();
   while ($stmt->fetch())
     $data[] = array("cid" => $cid, "user" => $user, "cmd" => $cmd, "timestamp" => $timestamp, "njobs" => $njobs);
-
-  echo json_encode($data);
+  $stmt->close();
 }
 else if ($_REQUEST['search'] == 'jobs') {
   $cluster_ids = $_REQUEST['clusterIds'];
 
-  $query = 'SELECT `cluster_id`, `proc_id`, `match_time`, IF(`success`, \'Yes\', \'No\'), `cputime`, `walltime`, `exitcode`';
-  $query .= ' FROM `jobs`';
-  $query .= sprintf(' WHERE `instance` = %d', $instance);
-  $query .= sprintf(' AND `cluster_id` IN (%s)', implode(',', $cluster_ids));
+  $query = 'SELECT c.`cluster_id`, j.`proc_id`, u.`name`, c.`cmd`, j.`match_time`, IF(j.`success`, \'Yes\', \'No\'), j.`cputime`, j.`walltime`, j.`exitcode`';
+  $query .= ' FROM `jobs` AS j';
+  $query .= ' INNER JOIN `job_clusters` AS c ON (c.`instance`, c.`cluster_id`) = (j.`instance`, j.`cluster_id`)';
+  $query .= ' INNER JOIN `users` AS u ON u.`user_id` = c.`user_id`';
+  $query .= sprintf(' WHERE c.`instance` = %d', $instance);
+  $query .= sprintf(' AND c.`cluster_id` IN (%s)', implode(',', $cluster_ids));
 
   $stmt = $db->prepare($query);
-  $stmt->bind_result($cid, $pid, $match_time, $success, $cputime, $walltime, $exitcode);
+  $stmt->bind_result($cid, $pid, $user, $cmd, $match_time, $success, $cputime, $walltime, $exitcode);
   $stmt->execute();
   while ($stmt->fetch())
     $data[] = array("cid" => $cid,
                     "pid" => $pid,
+                    "user" => $user,
+                    "cmd" => $cmd,
                     "matchTime" => $match_time,
                     "success" => $success,
                     "cputime" => $cputime,
                     "walltime" => $walltime,
                     "exitcode" => $exitcode);
-
-  echo json_encode($data);  
+  $stmt->close();
 }
+
+echo json_encode($data);
 
 ?>
