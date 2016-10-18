52a8384c67267b491e426723433ffc92fbe85542
Yutaro Iiyama <yiiyama@mit.edu>
2016-10-18 14:25:27 -0400

    fix

--- vomsinfo/install.sh
+++ vomsinfo/install.sh
@@ -7,5 +7,5 @@ cp $SRCDIR/vomsinfo /usr/local/bin/
 crontab -l | grep vomsinfo > /dev/null
 if [ $? -ne 0 ]
 then
-  (crontab -l; echo $SRCDIR/crontab) | crontab -
+  (crontab -l; cat $SRCDIR/crontab) | crontab -
 fi
