da83770a51c5cd86912fe7bc1ec834f81506c3ef
Christoph Paus <paus@mit.edu>
2017-04-26 15:40:11 -0400

    Python condor bindings for schedd.history broke. Introduced ugly patch.

--- condor_history/update.py
+++ condor_history/update.py
@@ -74,10 +74,46 @@ while len(open_clusters) != 0:
     # exhausted the list of open clusters; append "everything beyond"
     if len(open_clusters) == 0:
         constraints.append('ClusterId > %d' % ranges[-1][1])
-    
+
     constraint = classad.ExprTree('||'.join(constraints))
 
-    all_ads.extend(schedd.history(constraint, classad_attrs, -1))
+    ## old implementation                                                                                                                                                                                #all_ads.extend(schedd.history(constraint, classad_attrs, -1))
+    ## new implementation and ugly long implementation (python bindings broken)
+    action = 'condor_history -constraint \"%s\" -autoformat %s'%("True"," ".join(classad_attrs))
+    p = subprocess.Popen(action,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    (out, err) = p.communicate()
+    rc = p.returncode
+
+    results = []
+    for line in out.split("\n"):
+        values = line.split(" ")
+        line_dict = {}                                     # print " Line: %d (Class: %d)"%(len(values),len(classad_attrs))
+        if len(values) == len(classad_attrs):
+
+            for k,t,v in zip(classad_attrs,classad_attrs_type,values):
+                try:
+                    if   t == 's':
+                        line_dict[k] = v
+                    elif t == 'd':
+                        line_dict[k] = int(v)
+                    elif t == 'f':
+                        line_dict[k] = float(v)
+                    else:
+                        line_dict[k] = v
+                except:
+                   if k == 'ExitCode' and v == 'undefined':
+                        line_dict[k] = -1
+                    else:
+                        print ' ERROR == Exception: Attribute: %s  Type: %s  Value: %s'%(k,t,v)
+                        print line
+
+            results.append(line_dict)
+
+    # add all results to the all_adds array
+    #print " append more RESULTS"
+    all_ads.extend(results)
+
+
 
 # Some efficiency measures
 cluster_jobs = dict()
@@ -107,7 +143,7 @@ for jobads in all_ads:
             except KeyError:
                 # No User classad found
                 pass
-    
+
             if user == '' or user in is_nobody:
                 user_id = 0
 
@@ -121,21 +157,21 @@ for jobads in all_ads:
                         logger.info('Inserting user %s(%d)', user, user_id)
                         db_query('INSERT INTO `users` VALUES (%s, %s)', user_id, user)
                         users[user] = user_id
-    
+
                     except KeyError:
                         # User unknown to system password database
                         logger.warning('Unknown user %s', user)
 
                         is_nobody.add(user)
                         user_id = 0
-    
+
             # This is the submit time of this particular process and not of the cluster
             # but we are not interested in seconds-precision here; is a good enough approximation
             global_jobid = jobads['GlobalJobId']
             submit_time = time.localtime(int(global_jobid[global_jobid.rfind('#') + 1:]))
 
             logger.info('Inserting cluster (%d, %s, %s, %s)', cluster_id, user, time.strftime('%Y-%m-%d %H:%M:%S', submit_time), os.path.basename(jobads['Cmd'])[:16])
-    
+
             # Now insert the cluster information
             db_query('INSERT INTO `job_clusters` VALUES (%s, %s, %s, %s, %s)', CONDOR_INSTANCE, cluster_id, user_id, time.strftime('%Y-%m-%d %H:%M:%S', submit_time), os.path.basename(jobads['Cmd'])[:16])
 
@@ -210,7 +246,7 @@ for jobads in all_ads:
         wall_time = -1
 
     logger.info('Inserting job %d.%d (success: %d)', cluster_id, proc_id, success)
-    
+
     db_query('INSERT INTO `jobs` VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
              CONDOR_INSTANCE,
              cluster_id,
@@ -221,7 +257,7 @@ for jobads in all_ads:
              cpu_time,
              wall_time,
              exit_code)
-    
+
     cluster_jobs[cluster_id].add(proc_id)
 
 # save currently open clusters
